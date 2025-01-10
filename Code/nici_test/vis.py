8b7665c128c86c6893666c031a728473fa4d39e2
studkreudo3099 <dominic.kreuz@stud.th-rosenheim.de>
2025-01-10 23:57:00 +0100

    mined missing data, gdp data now complete

---
+++ Code/nici_test/vis.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from data_utils import load_data  # Changed import statement
+from plot_utils import plot_panel1, plot_panel2
+
+
+# TODO: https://plotly.com/python/range-slider/
+# TODO: decide on styling (colors, marks, etc.)
+
+
+data_dir = Path(__file__).parent.parent.parent / 'data'
+inflation_path = data_dir / 'inflation/inflation_de_sl.csv'
+GDP_path = data_dir / 'gdp/gdp_de_sl.csv'
+happiness_path = data_dir / 'happiness/happiness_de_sl.csv'
+tourism_path = data_dir / 'tourism/tourism_de_sl.csv'
+
+
+data = load_data(inflation_path, GDP_path, happiness_path, tourism_path)
+plot_panel1(data)
+plot_panel2(data)
